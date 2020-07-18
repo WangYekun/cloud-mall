f4a0a1241a4a7c5cf34411a89a1336fe0b5e1304
WangYekun <849907942@qq.com>
2020-07-18 23:36:28 +0800

    [feat] init project

---
+++ conpon-service/src/test/java/com/mall/conpons/ConponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.conpons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
